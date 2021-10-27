7411fd02a59c0c1efc2ad46bdbc5482c8ba48522
paobai <2521230598@qq.com>
2021-10-27 10:22:57 +0800

    提交结构修改。

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import App from './App.vue'
 import router from './router'
 import { store } from './store'
 // import { loadAllPlugins } from './plugins'
-import { i18n } from '@/plugins/i18n';
-import { useElementPlus } from "@/plugins/element-plus";
+import { i18n } from '@/plugins/i18n'
+import { useElementPlus } from '@/plugins/element-plus'
 import { useArco } from '@/plugins/arco'
 import * as directives from '@/directives'
 import '@/assets/iconfont/iconfont.css'
@@ -14,12 +14,8 @@ const app = createApp(App)
 // loadAllPlugins(app)
 
 // 自定义指令
-Object.keys(directives).forEach(key => {
-    app.directive(key, (directives as { [key: string ]: Directive })[key])
+Object.keys(directives).forEach((key) => {
+  app.directive(key, (directives as { [key: string]: Directive })[key])
 })
-app.use(router)
-    .use(useArco)
-    .use(useElementPlus)
-    .use(i18n)
-    .use(store)
+app.use(router).use(useArco).use(useElementPlus).use(i18n).use(store)
 app.mount('#app')
--- src/router/index.ts
+++ src/router/index.ts
@@ -5,23 +5,23 @@
  * @LastEditors: ZY
  * @LastEditTime: 2021-01-27 20:10:59
  */
-import {createRouter, createWebHashHistory, RouteRecordRaw} from 'vue-router'
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import {
   buildRouter,
   fixMenu,
   fnCurrentRouteType,
   GetRouteStructure,
   MyRouter,
-  MyRouterOptions
-} from "@/router/routerHelp";
+  MyRouterOptions,
+} from '@/router/routerHelp'
 import Layout from '@/layout/Index.vue'
-import {getCurrentUserTree as getCurrentUserTreeApi} from '@/api/upms-api'
-import {useStore} from '@/store'
-import {LoginType} from "@/store/modules/paobai/state";
+import { getCurrentUserTree as getCurrentUserTreeApi } from '@/api/upms-api'
+import { useStore } from '@/store'
+import { LoginType } from '@/store/modules/paobai/state'
 import Cookies from 'js-cookie'
-import {PermissionActionType} from "@/store/modules/permission/action-types";
+import { PermissionActionType } from '@/store/modules/permission/action-types'
 
-const modulesRoutes = import.meta.glob("/src/views/*/*.vue");
+const modulesRoutes = import.meta.glob('/src/views/*/*.vue')
 
 const store = useStore()
 
@@ -37,7 +37,6 @@ Object.keys(commonFiles).forEach((key) => {
 //   commonModules = commonModules.concat(otherFiles[key].default)
 // })
 
-
 // 主入口路由(需嵌套上左右整体布局)
 const mainRoutes: RouteRecordRaw = {
   path: '/',
@@ -46,15 +45,16 @@ const mainRoutes: RouteRecordRaw = {
   children: [
     {
       path: '/main',
-      component: () => import(/* webpackChunkName: "dashboard" */ '@/views/main.vue'),
+      component: () =>
+        import(/* webpackChunkName: "dashboard" */ '@/views/main.vue'),
       name: 'main',
       meta: {
         title: 'main',
         icon: '#icondashboard',
-        affix: true
-      }
-    }
-  ]
+        affix: true,
+      },
+    },
+  ],
 }
 
 export const constantRoutes: Array<RouteRecordRaw> = [
@@ -70,23 +70,23 @@ export const constantRoutes: Array<RouteRecordRaw> = [
   //   ]
   // },
   mainRoutes,
-  ...commonModules
+  ...commonModules,
 ]
 
 const routerOptions: MyRouterOptions = {
   history: createWebHashHistory(),
   routes: constantRoutes,
-  isAddDynamicMenuRoutes: false
+  isAddDynamicMenuRoutes: false,
 }
-const router:MyRouter = createRouter(routerOptions)
+const router: MyRouter = createRouter(routerOptions)
 router.addRoute({
-  path: "/:pathMatch(.*)",
-  redirect: "/error/404"
-});
+  path: '/:pathMatch(.*)',
+  redirect: '/error/404',
+})
 
 export function resetRouter() {
-  const newRouter = router;
-  (router as any).matcher = (newRouter as any).matcher // reset router
+  const newRouter = router
+  ;(router as any).matcher = (newRouter as any).matcher // reset router
 }
 
 router.beforeEach((to, from, next) => {
@@ -94,48 +94,56 @@ router.beforeEach((to, from, next) => {
   // 1. 已经添加 or 全局路由, 直接访问
   // 2. 获取菜单列表, 添加并保存本地存储
   // debugger
-  if (router.options.isAddDynamicMenuRoutes || fnCurrentRouteType(to as any, commonModules) === 'global') {
+  if (
+    router.options.isAddDynamicMenuRoutes ||
+    fnCurrentRouteType(to as any, commonModules) === 'global'
+  ) {
     next()
   } else {
     // TODO 获取路由
-    let token = Cookies.get('token')
+    const token = Cookies.get('token')
     if (!token) {
       router.push({ name: 'login' })
       next()
       return
     }
-    getCurrentUserTreeApi().then((res) => {
-      if (res.code === 1) {
-        let newRoute: Array<GetRouteStructure> = res.data as any
-        newRoute.forEach(item => {
-          buildRouter(item, modulesRoutes, router)
-          // Dynamically add accessible routes
-        })
-        // store.state.permission.dynamicRoutes.forEach((route) => {
-        //   router.addRoute(route)
-        // })
-        store.dispatch(PermissionActionType.ACTION_SET_ROUTES, newRoute)
-      }
-      next()
-      // console.log(res)
-      // if (res && res.code === 0) {
-      //   let menus = res.data
-      //   let fixRes = fixMenu(menus)
-      //   router.options.isAddDynamicMenuRoutes = true
-      //   // let btPerms = fixRes.btPerms.map(item => item.menuKey)
-      //   // store.commit('user/updateBtPerms', btPerms)
-      //   sessionStorage.setItem('menuList', JSON.stringify(menus || '[]'))
-      //   // sessionStorage.setItem('permissions', JSON.stringify(res.data.permissions || '[]'))
-      //   next({ ...to, replace: true })
-      // } else {
-      //   sessionStorage.setItem('menuList', '[]')
-      //   // sessionStorage.setItem('permissions', '[]')
-      //   next()
-      // }
-    }).catch((e) => {
-      console.log(`%c${e} 请求菜单列表和权限失败，跳转至登录页！！`, 'color:blue')
-      router.push({ name: 'login' })
-    })
+    getCurrentUserTreeApi()
+      .then((res) => {
+        if (res.code === 1) {
+          const newRoute: Array<GetRouteStructure> = res.data as any
+          newRoute.forEach((item) => {
+            buildRouter(item, modulesRoutes, router)
+            // Dynamically add accessible routes
+          })
+          // store.state.permission.dynamicRoutes.forEach((route) => {
+          //   router.addRoute(route)
+          // })
+          store.dispatch(PermissionActionType.ACTION_SET_ROUTES, newRoute)
+        }
+        next()
+        // console.log(res)
+        // if (res && res.code === 0) {
+        //   let menus = res.data
+        //   let fixRes = fixMenu(menus)
+        //   router.options.isAddDynamicMenuRoutes = true
+        //   // let btPerms = fixRes.btPerms.map(item => item.menuKey)
+        //   // store.commit('user/updateBtPerms', btPerms)
+        //   sessionStorage.setItem('menuList', JSON.stringify(menus || '[]'))
+        //   // sessionStorage.setItem('permissions', JSON.stringify(res.data.permissions || '[]'))
+        //   next({ ...to, replace: true })
+        // } else {
+        //   sessionStorage.setItem('menuList', '[]')
+        //   // sessionStorage.setItem('permissions', '[]')
+        //   next()
+        // }
+      })
+      .catch((e) => {
+        console.log(
+          `%c${e} 请求菜单列表和权限失败，跳转至登录页！！`,
+          'color:blue'
+        )
+        router.push({ name: 'login' })
+      })
   }
 })
 
