11ec3245616e558bf5bb067835520595a16764a0
paobai <2521230598@qq.com>
2022-01-12 17:02:19 +0800

    增加修改系统颜色配置的方法。

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -6,8 +6,8 @@ import { storageSession } from '@/utils/storage'
 import sysConfig from "@/config";
 
 export function clearLoginInfo() {
-  Cookies.remove(sysConfig.tokenName)
-  Cookies.remove(sysConfig.refreshTokenName)
+  Cookies.remove(sysConfig.app.tokenName)
+  Cookies.remove(sysConfig.app.refreshTokenName)
   // store.commit('resetStore')
   // @ts-ignore
   storageSession.setItem(sysConfig.permissionName, []);
@@ -29,4 +29,33 @@ export const toggleClass = (ele: HTMLElement, className: string) => {
       classString.substr(nameIndex + className.length)
   }
   ele.className = classString
+}
+
+/**
+ * 修改
+ * @param color 颜色
+ * @param propertyPre 更新类型默认主题色
+ */
+export const changeArcoPrimaryColor = (color: string, propertyPre='--arcoblue-') => {
+  const tintColor = (color: string, tint: number) => {
+    let red = parseInt(color.slice(0, 2), 16)
+    let green = parseInt(color.slice(2, 4), 16)
+    let blue = parseInt(color.slice(4, 6), 16)
+
+    if (tint === 0) { // when primary color is in its rgb space
+      return [red, green, blue].join(',')
+    } else {
+      red += Math.round(tint * (255 - red))
+      green += Math.round(tint * (255 - green))
+      blue += Math.round(tint * (255 - blue))
+      return `${red},${green},${blue}`
+    }
+  }
+  const clusters = []
+  let body = document.body
+  for (let i = 0; i <= 9; i++) {
+    let tgbText = tintColor(color.replace('#', ''), Number(((i - 5) / 10).toFixed(2)))
+    clusters.push(tgbText)
+    body.style.setProperty(propertyPre + (i+1), tgbText)
+  }
 }
