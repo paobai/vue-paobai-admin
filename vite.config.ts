edbf4875ea45ae44226f9e98f23b2caae7ddc1d4
paobai <2521230598@qq.com>
2022-02-18 14:28:18 +0800

    修改vite.config.ts的elint

--- vite.config.ts
+++ vite.config.ts
@@ -6,55 +6,52 @@
 //   plugins: [vue()]
 // })
 
-import { resolve } from "path";
-import { UserConfigExport, ConfigEnv, loadEnv } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
-import { warpperEnv } from "./build/utils";
-import { createProxy } from "./build/proxy";
-import { viteMockServe } from "vite-plugin-mock";
-import styleImport from "vite-plugin-style-import";
+import { resolve } from "path"
+import { UserConfigExport, ConfigEnv, loadEnv } from "vite"
+import vue from "@vitejs/plugin-vue"
+import vueJsx from "@vitejs/plugin-vue-jsx"
+import { warpperEnv } from "./build/utils"
+import { createProxy } from "./build/proxy"
+import { viteMockServe } from "vite-plugin-mock"
+import styleImport from "vite-plugin-style-import"
 
 const pathResolve = (dir: string): any => {
-  return resolve(__dirname, ".", dir);
-};
+  return resolve(__dirname, ".", dir)
+}
 
 const alias: Record<string, string> = {
   "@": pathResolve("src"),
   //解决开发环境下的警告 You are running the esm-bundler build of vue-i18n. It is recommended to configure your bundler to explicitly replace feature flag globals with boolean literals to get proper tree-shaking in the final bundle.
-  "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js",
-};
+  "vue-i18n": "vue-i18n/dist/vue-i18n.cjs.js"
+}
 
-const root: string = process.cwd();
+const root: string = process.cwd()
 
 export default ({ command, mode }: ConfigEnv): UserConfigExport => {
-    const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = warpperEnv(
-        loadEnv(mode, root)
-    );
-  let prodMock = true;
+  const { VITE_PORT, VITE_PUBLIC_PATH, VITE_PROXY } = warpperEnv(
+    loadEnv(mode, root)
+  )
+  const prodMock = true
   return {
     /**
      * 基本公共路径
      * /manages/ 可根据项目部署域名的后缀自行填写（全局搜/manages/替换）
      * @default '/'
      */
-    base:
-        process.env.NODE_ENV === "production" ? "./" : VITE_PUBLIC_PATH,
+    base: process.env.NODE_ENV === "production" ? "./" : VITE_PUBLIC_PATH,
     root,
     resolve: {
-      alias,
+      alias
     },
     css: {
       preprocessorOptions: {
-        scss: {
-        },
+        scss: {},
         less: {
           additionalData: '@import "./src/styles/_variables.module.less";',
-          modifyVars: {
-          },
+          modifyVars: {},
           javascriptEnabled: true
         }
-      },
+      }
     },
     // 服务端渲染
     server: {
@@ -64,10 +61,10 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
        * 端口号
        * @default 3000
        */
-      host: '0.0.0.0',
+      host: "0.0.0.0",
       port: VITE_PORT,
       // 本地跨域代理
-      proxy: createProxy(VITE_PROXY),
+      proxy: createProxy(VITE_PROXY)
     },
     plugins: [
       vue(),
@@ -75,39 +72,38 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       styleImport({
         libs: [
           {
-            libraryName: '@arco-design/web-vue',
+            libraryName: "@arco-design/web-vue",
             esModule: true,
-            resolveStyle: (name) => {
+            resolveStyle: name => {
               // css
               return `@arco-design/web-vue/es/${name}/style/css.js`
               // less
               return `@arco-design/web-vue/es/${name}/style/index.js`
-            },
+            }
           }
-        ],
+        ]
       }),
       viteMockServe({
         mockPath: "mock",
-          localEnabled: command === "serve",
-          prodEnabled: command !== "serve" && prodMock,
+        localEnabled: command === "serve",
+        prodEnabled: command !== "serve" && prodMock,
         injectCode: `
           import { setupProdMockServer } from './mockProdServer';
           setupProdMockServer();
         `,
-        logger: true,
-      }),
+        logger: true
+      })
     ],
     optimizeDeps: {
-      include: [
-      ],
+      include: []
     },
     build: {
       brotliSize: false,
       // 消除打包大小超过500kb警告
-      chunkSizeWarningLimit: 2000,
+      chunkSizeWarningLimit: 2000
     },
     define: {
-      __INTLIFY_PROD_DEVTOOLS__: false,
-    },
-  };
-};
+      __INTLIFY_PROD_DEVTOOLS__: false
+    }
+  }
+}
