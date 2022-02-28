8ef79d7735428af90485248794dcd7283f0e344f
paobai <2521230598@qq.com>
2022-02-28 17:48:00 +0800

    调整mock配置

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,5 +5,6 @@ module.exports = {
     arrowParens: 'avoid',
     trailingComma: 'none',
     printWidth: 200, // 换行长度
+    endOfLine: 'auto', //避免报错delete (cr)的错 文件换行不同系统不一样。 LF CRLF
     semi: false  // 句尾不使用分号
   }
