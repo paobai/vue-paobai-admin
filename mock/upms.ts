efab1d4bbc77bbbde7aaf30377464411b8fceb42
paobai <2521230598@qq.com>
2022-04-17 22:30:24 +0800

    feat: 增加不同角色的权限控制。

--- mock/upms.ts
+++ mock/upms.ts
@@ -3,192 +3,215 @@ import { RouteType } from "@/constant/settings"
 import { failResponseWrap, successResponseWrap } from "@/utils/mock"
 
 const urlPre = "/upms"
+const paobaiUserInfo = {
+  userId: 1,
+  userName: "paobai",
+  realName: "泡白",
+  passWord: "paobai",
+  token: "paobaiToken",
+  roles: ["admin"]
+}
+const testUserInfo = {
+  userId: 2,
+  userName: "test",
+  realName: "测试用户",
+  passWord: "test",
+  token: "testToken",
+  roles: []
+}
+function getUserId(request: any) {
+  return request.headers.cookie.includes(testUserInfo.token) ? testUserInfo.userId : paobaiUserInfo.userId
+}
 
 export default [
   {
     url: urlPre + "/users/current/menus",
     method: "get",
-    response: () => {
+    response: (request: any) => {
+      const commonRouters = [
+        {
+          title: "Arco Design Pro",
+          path: "",
+          key: "menu-header-1",
+          type: RouteType.Menu,
+          icon: "iconfont-store-fill",
+          sort: 2,
+          children: [
+            {
+              key: "workplace",
+              title: "工作台",
+              type: RouteType.Page,
+              icon: "iconfont-apparel",
+              path: "/dashboard/workplace/index"
+            },
+            {
+              key: "monitor",
+              title: "实时监控",
+              type: RouteType.Page,
+              path: "/dashboard/monitor/index"
+            },
+            {
+              key: "visualization",
+              title: "分析数据可视化",
+              type: RouteType.Menu,
+              icon: "iconfont-integral",
+              children: [
+                {
+                  key: "data-analysis",
+                  title: "数据分析",
+                  type: RouteType.Page,
+                  path: "/visualization/data-analysis/index"
+                },
+                {
+                  key: "multi-dimension-data-analysis",
+                  title: "多维数据分析",
+                  type: RouteType.Page,
+                  path: "/visualization/multi-dimension-data-analysis/index"
+                }
+              ]
+            },
+            {
+              title: "其他页面",
+              path: "http://vue-pro.arco.design/",
+              key: "arco-design-pro",
+              icon: "iconfont-homepage-ads-fill",
+              type: RouteType.Iframe
+            }
+          ]
+        },
+        {
+          title: "多菜单",
+          path: "",
+          key: "menu-multi",
+          icon: "iconfont-all-fill",
+          type: RouteType.Menu,
+          children: [
+            {
+              key: "1",
+              title: "menu1",
+              type: RouteType.Page,
+              path: "/test-router/menu1"
+            },
+            {
+              key: "2",
+              title: "menu2",
+              type: RouteType.Menu,
+              children: [
+                {
+                  key: "2_1",
+                  title: "menu2-1",
+                  type: RouteType.Page,
+                  path: "/test-router/menu2/menu2-1"
+                },
+                {
+                  key: "2_2",
+                  title: "menu2-2",
+                  type: RouteType.Page,
+                  path: "/test-router/menu2/menu2-2"
+                }
+              ]
+            },
+            {
+              key: "3",
+              title: "menu3",
+              type: RouteType.Menu,
+              children: [
+                {
+                  key: "3_1",
+                  title: "menu3-1",
+                  type: RouteType.Menu,
+                  children: [
+                    {
+                      key: "3_1_1",
+                      title: "menu3-1-1",
+                      type: RouteType.Page,
+                      path: "/test-router/menu3/menu3-1/menu3-1-1"
+                    },
+                    {
+                      key: "3_2_2",
+                      title: "menu3-1-2",
+                      type: RouteType.Page,
+                      path: "/test-router/menu3/menu3-1/menu3-1-2"
+                    }
+                  ]
+                },
+                {
+                  key: "3_2",
+                  title: "menu3-2",
+                  type: RouteType.Page,
+                  path: "/test-router/menu3/menu3-2"
+                },
+                {
+                  key: "3_3",
+                  title: "menu3-3",
+                  type: RouteType.Page,
+                  path: "/test-router/menu3/menu3-3"
+                }
+              ]
+            },
+            {
+              key: "cd2-3_1",
+              title: "菜单2的页面1",
+              type: RouteType.Page,
+              path: ""
+            },
+            {
+              key: "cd2-3_2",
+              title: "菜单2的页面2",
+              type: RouteType.Page,
+              path: ""
+            }
+          ]
+        },
+        {
+          title: "单页面",
+          path: "/test-router/single-page",
+          key: "menu-single",
+          icon: "iconfont-homepage-ads-fill",
+          type: RouteType.Page
+        },
+        {
+          title: "Iframe页面",
+          path: "https://arco.design/vue/docs/pro/start",
+          key: "menu-iframe",
+          icon: "iconfont-homepage-ads-fill",
+          type: RouteType.Iframe
+        },
+        {
+          title: "用户设置",
+          path: "",
+          key: "menu-user-setting",
+          icon: "iconfont-set1",
+          type: RouteType.Menu,
+          children: [
+            {
+              title: "权限设置",
+              path: "/user-setting/authority-setting/index",
+              key: "authority-setting",
+              type: RouteType.Page,
+              icon: "iconfont-fencengpeizhi"
+            },
+            {
+              title: "低权限页面",
+              key: "authority-setting-low-power-page",
+              type: RouteType.Page,
+              icon: "iconfont-component"
+            }
+          ]
+        }
+      ]
       const data = {
-        routers: [
-          {
-            title: "Arco Design Pro",
-            path: "",
-            key: "menu-header-1",
-            type: RouteType.Menu,
-            icon: "iconfont-store-fill",
-            sort: 2,
-            children: [
-              {
-                key: "workplace",
-                title: "工作台",
-                type: RouteType.Page,
-                icon: "iconfont-apparel",
-                path: "/dashboard/workplace/index"
-              },
-              {
-                key: "monitor",
-                title: "实时监控",
-                type: RouteType.Page,
-                path: "/dashboard/monitor/index"
-              },
-              {
-                key: "visualization",
-                title: "分析数据可视化",
-                type: RouteType.Menu,
-                icon: "iconfont-integral",
-                children: [
-                  {
-                    key: "data-analysis",
-                    title: "数据分析",
-                    type: RouteType.Page,
-                    path: "/visualization/data-analysis/index"
-                  },
-                  {
-                    key: "multi-dimension-data-analysis",
-                    title: "多维数据分析",
-                    type: RouteType.Page,
-                    path: "/visualization/multi-dimension-data-analysis/index"
-                  }
-                ]
-              },
-              {
-                title: "其他页面",
-                path: "http://vue-pro.arco.design/",
-                key: "arco-design-pro",
-                icon: "iconfont-homepage-ads-fill",
-                type: RouteType.Iframe
-              }
-            ]
-          },
-          {
-            title: "多菜单",
-            path: "",
-            key: "menu-multi",
-            icon: "iconfont-all-fill",
-            type: RouteType.Menu,
-            children: [
-              {
-                key: "1",
-                title: "menu1",
-                type: RouteType.Page,
-                path: "/test-router/menu1"
-              },
-              {
-                key: "2",
-                title: "menu2",
-                type: RouteType.Menu,
-                children: [
-                  {
-                    key: "2_1",
-                    title: "menu2-1",
-                    type: RouteType.Page,
-                    path: "/test-router/menu2/menu2-1"
-                  },
-                  {
-                    key: "2_2",
-                    title: "menu2-2",
-                    type: RouteType.Page,
-                    path: "/test-router/menu2/menu2-2"
-                  }
-                ]
-              },
-              {
-                key: "3",
-                title: "menu3",
-                type: RouteType.Menu,
-                children: [
-                  {
-                    key: "3_1",
-                    title: "menu3-1",
-                    type: RouteType.Menu,
-                    children: [
-                      {
-                        key: "3_1_1",
-                        title: "menu3-1-1",
-                        type: RouteType.Page,
-                        path: "/test-router/menu3/menu3-1/menu3-1-1"
-                      },
-                      {
-                        key: "3_2_2",
-                        title: "menu3-1-2",
-                        type: RouteType.Page,
-                        path: "/test-router/menu3/menu3-1/menu3-1-2"
-                      }
-                    ]
-                  },
-                  {
-                    key: "3_2",
-                    title: "menu3-2",
-                    type: RouteType.Page,
-                    path: "/test-router/menu3/menu3-2"
-                  },
-                  {
-                    key: "3_3",
-                    title: "menu3-3",
-                    type: RouteType.Page,
-                    path: "/test-router/menu3/menu3-3"
-                  }
-                ]
-              },
-              {
-                key: "cd2-3_1",
-                title: "菜单2的页面1",
-                type: RouteType.Page,
-                path: ""
-              },
-              {
-                key: "cd2-3_2",
-                title: "菜单2的页面2",
-                type: RouteType.Page,
-                path: ""
-              }
-            ]
-          },
-          {
-            title: "单页面",
-            path: "/test-router/single-page",
-            key: "menu-single",
-            icon: "iconfont-homepage-ads-fill",
-            type: RouteType.Page
-          },
-          {
-            title: "Iframe页面",
-            path: "https://arco.design/vue/docs/pro/start",
-            key: "menu-iframe",
-            icon: "iconfont-homepage-ads-fill",
-            type: RouteType.Iframe
-          },
-          {
-            title: "用户设置",
-            path: "",
-            key: "menu-user-setting",
-            icon: "iconfont-set1",
-            type: RouteType.Menu,
-            children: [
-              {
-                title: "权限设置",
-                path: "/user-setting/authority-setting/index",
-                key: "authority-setting",
-                type: RouteType.Page,
-                icon: "iconfont-fencengpeizhi"
-              },
-              {
-                title: "高权限页面",
-                key: "authority-setting-hight-level",
-                type: RouteType.Page,
-                icon: "iconfont-component"
-              },
-              {
-                title: "低权限页面",
-                key: "authority-setting-low-level",
-                type: RouteType.Page,
-                icon: "iconfont-component"
-              }
-            ]
-          }
-        ],
-        permissions: []
+        routers: commonRouters,
+        permissions: ["low-power-button"]
+      }
+      if (getUserId(request) === paobaiUserInfo.userId) {
+        data.routers[4].children.push({
+          title: "高权限页面",
+          key: "authority-setting-high-power-page",
+          type: RouteType.Page,
+          icon: "iconfont-component"
+        })
+        data.permissions.push("high-power-button")
       }
       return successResponseWrap(data)
     }
@@ -199,11 +222,15 @@ export default [
     // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
     response: (request: any) => {
       const data = request.body
-      if (data.userName !== "paobai" || data.passWord !== "paobai") {
-        return failResponseWrap(null, "错误账号或者密码")
-      }
       const res = {
-        access_token: "123123"
+        access_token: ""
+      }
+      if (data.userName === paobaiUserInfo.userName && data.passWord === paobaiUserInfo.passWord) {
+        res.access_token = paobaiUserInfo.token
+      } else if (data.userName === testUserInfo.userName && data.passWord === testUserInfo.passWord) {
+        res.access_token = testUserInfo.token
+      } else {
+        return failResponseWrap(null, "错误账号或者密码")
       }
       return successResponseWrap(res)
     }
@@ -214,12 +241,18 @@ export default [
     // eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars
     response: (request: any) => {
       const res = {
-        userId: 1,
-        username: "paobai",
-        realName: "泡白",
+        userId: paobaiUserInfo.userId,
+        username: paobaiUserInfo.userName,
+        realName: paobaiUserInfo.realName,
         avatar: "./images/avatar.jpg",
-        roles: ["admin"],
+        roles: paobaiUserInfo.roles,
         request: request
+      }
+      if (getUserId(request) === testUserInfo.userId) {
+        res.realName = testUserInfo.realName
+        res.username = testUserInfo.userName
+        res.roles = testUserInfo.roles
+        res.userId = testUserInfo.userId
       }
       return successResponseWrap(res)
     }
--- src/views/modules/user-setting/authority-setting/index.vue
+++ src/views/modules/user-setting/authority-setting/index.vue
@@ -1,11 +1,63 @@
 <template>
-  <div>iframe</div>
+  <div class="authority-setting">
+    <h3>高级权限用户：用户名:paobai 密码: paobai</h3>
+    <h3>低级权限用户： 用户名:test 密码: paobai</h3>
+    <h3>查看菜单中高权限和低权限的页面显隐控制</h3>
+    <h2>当前用户为：{{ userInfo.realName }}</h2>
+    <div>
+      <h3>切换用户：</h3>
+      <a-button size="large" type="primary" @click="changeUser('paobai')">切换为高级用户</a-button>
+      <a-button size="large" type="primary" style="margin-left: 20px" @click="changeUser('test')">切换为低级用户</a-button>
+    </div>
+    <div style="margin-top: 40px">
+      <h3>权限控制显隐按钮：</h3>
+      <a-button v-permission="'high-power-button'" type="primary" style="margin-right: 20px">高级权限可以看按钮（元素）</a-button>
+      <a-button v-permission="'low-power-button'" type="primary">低级权限也可以看按钮（元素）</a-button>
+    </div>
+  </div>
 </template>
 
-<script>
+<script lang="ts">
+import { useUserHook } from "@/hooks/user"
+import { AuthLoginByPasswordReq } from "@/api/auth-api/model"
+import { grantType } from "@/constant"
+import { AuthApi } from "@/api/auth-api"
+import Cookies from "@/utils/storage/cookie"
+import config from "@/config"
 export default {
-  name: "authority-setting"
+  name: "authority-setting",
+  setup() {
+    const { userInfo } = useUserHook()
+    const changeUser = function (userName: string) {
+      let loginForm: AuthLoginByPasswordReq = {
+        userName: userName,
+        passWord: userName,
+        grant_type: grantType.PASSWORD
+      }
+      const login = async () => {
+        AuthApi.login(loginForm).then(res => {
+          Cookies.set(config.app.tokenName, res.data.access_token)
+          Cookies.set(config.app.refreshTokenName, res.data.refresh_token)
+          location.reload()
+        })
+      }
+      login()
+    }
+    return {
+      userInfo,
+      changeUser
+    }
+  }
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="less">
+.authority-setting {
+  width: calc(100% - 40px);
+  height: calc(100% - 40px);
+  background: #ffffff;
+  padding: 20px;
+  margin: 20px;
+  line-height: 40px;
+}
+</style>
