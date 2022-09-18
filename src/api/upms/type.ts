6f9e97a3d1e4114343fb5a7fd0a474d79e7720cd
paobai <2521230598@qq.com>
2022-09-18 16:12:16 +0800

    revert: delete unused files

--- src/api/upms/type.ts
+++ src/api/upms/type.ts
@@ -35,22 +35,6 @@ export namespace RoleApiType {
   }
 }
 
-export namespace ResourceApiType {
-  export type resourceItem = {
-    groupId: AppIdType
-    id: AppIdType
-    lockFlag: boolean
-    permissionCode: string
-    resourceName: string
-    sort?: number
-  }
-  export type resourceGroupItem = {
-    children: resourceItem[]
-    groupId: AppIdType
-    groupName: string
-  }
-}
-
 export namespace MenuApiType {
   export type get = RouterApiType[]
 }
